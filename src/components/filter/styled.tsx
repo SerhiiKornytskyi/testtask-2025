9b3fde22f4f1e50d3a2bd04faedbb0dac610c69f
Serhii_Kornytskiy <protectr20@gmail.com>
2025-09-28 16:30:21 +0200

    detail pages complete

---
+++ src/components/filter/styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  text-align: center;
+  justify-content: center;
+  color: white;
+  h2 {
+    display: block;
+    margin-bottom: 20px;
+  }
+  .imgWrapper {
+    display: block;
+    width: 100%;
+  }
+`;
